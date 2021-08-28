aeaa417dbfe038e5bb827a87c3efee1a6e9c52cb
Paul Onyebuchi <70877999+ecoderP@users.noreply.github.com>
2021-08-28 09:09:25 +0100

    update -
    Add more toggle and responsive functionalities to the sidebar navigation.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,66 @@
 // Toggle left harmbugger menus
 const openMenu = document.getElementById('open-menu');
 const closeMenu = document.getElementById('close-menu');
+const logo = document.querySelector('.bx-doughnut-chart');
+const codeVotex = document.querySelector('.codevotex');
+
 
 openMenu.addEventListener('click', open);
 closeMenu.addEventListener('click', close);
 
 function open() {
+    
     if(openMenu.style.display === 'none') {
         openMenu.style.display = 'block';
-        closeMenu.style.display = 'none'
+        closeMenu.style.display = 'none';
+        logo.style.display = 'none';
+        codeVotex.style.display = 'none';
+        
     } else {
         openMenu.style.display = 'none';
-        closeMenu.style.display = 'block'
+        closeMenu.style.display = 'block';
+        logo.style.display = 'block';
+        codeVotex.style.display = 'block';
+        
     }
+    minimiseSideBar();
 }
 function close() {
     if(closeMenu.style.display === 'none') {
         closeMenu.style.display = 'block';
-        openMenu.style.display = 'none'
+        openMenu.style.display = 'none';
+        logo.style.display = 'block';
+        codeVotex.style.display = 'block';
     } else {
         closeMenu.style.display = 'none';
-        openMenu.style.display = 'block'
+        openMenu.style.display = 'block';
+        logo.style.display = 'none';
+        codeVotex.style.display = 'none';
+    }
+    minimiseSideBar()
+}
+function minimiseSideBar() {
+    const hideSideLinks = document.querySelectorAll('.link-item');
+    const sideBar = document.getElementById('left-sidebar');
+    const searchBtn = document.getElementById('search-btn');
+    
+    for(i=0; i < hideSideLinks.length; i++) {
+        if(openMenu.style.display === 'block') {
+            hideSideLinks[i].style.display = 'none';
+        } else {
+            hideSideLinks[i].style.display = 'block';
+        }
+        
+     }
+     if(sideBar.classList.contains('col-md-3')) {
+        sideBar.classList.replace('col-md-3', 'col-md-1');
+        searchBtn.classList.add('open-state')
+    } else {
+        sideBar.classList.replace('col-md-1', 'col-md-3');
+        searchBtn.classList.remove('open-state')
     }
 }
+
 
 // Toggle mobile menu on right hand side
 const horizontalDots = document.getElementById('horizontalDots');
@@ -31,7 +69,6 @@ const mobileRightHandMenu = document.getElementById('mobile-menu');
 horizontalDots.addEventListener('click', openRightHandMenu);
 
 function openRightHandMenu() {
-    console.log(2);
     if(horizontalDots.classList.contains('horizontal-dots')) {
         horizontalDots.classList.remove('horizontal-dots');
         mobileRightHandMenu.style.display = 'none'
